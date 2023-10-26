76c5edbe1c64fc331d9a90ba79bc402ed059d61f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-10-26 19:11:27 +0000

    add remote context proto

--- platform-server/src/RemoteServer.js
+++ platform-server/src/RemoteServer.js
@@ -1,5 +1,49 @@
-class RemoteServer {
+import { ApplicationService } from '@themost/common';
+import path from 'path';
+import { Server, loadPackageDefinition, ServerCredentials } from '@grpc/grpc-js';
+import {loadSync} from '@grpc/proto-loader';
+import { ODataModelBuilder } from '@themost/data';
 
+
+class RemoteServer extends ApplicationService {
+    constructor(app) {
+        super(app);
+        const PROTO_PATH = path.resolve(__dirname, '../../protos/remote_context.proto');
+        const packageDefinition = loadSync(
+            PROTO_PATH,
+            {
+                keepCase: true,
+                longs: String,
+                enums: String,
+                defaults: true,
+                oneofs: true
+            });
+        this.remoting = loadPackageDefinition(packageDefinition).remoting;
+
+        const remoteServer = this.getServer();
+        remoteServer.bindAsync('localhost:50051', ServerCredentials.createInsecure(), () => {
+            remoteServer.start();
+        });
+    }
+
+    getServer() {
+        this.server = new Server();
+        this.server.addService(this.remoting.RemoteContext.service, {
+            getModel: (call, callback) => {
+                const name = call.request.name;
+                const service = this.application.getService(ODataModelBuilder);
+                const items = service.getEntity(name).property;
+                const property = items.map(({name, isBound}) => {
+                     name,
+                     isBound
+                });
+                return callback({
+                    property
+                });
+            },
+        });
+        return this.server;
+    }
 }
 
 export {
--- spec/RemoteContext.spec.js
+++ spec/RemoteContext.spec.js
@@ -1,6 +1,21 @@
 import { RemoteContext } from '@themost/remoting';
+import { RemoteServer } from '@themost/remoting/platform-server';
+import { getApplication, serveApplication } from '@themost/test';
 
 describe('RemoteContext', () => {
+
+    beforeAll((done) => {
+        const container = getApplication();
+        /**
+         * @type {import('@themost/express').ExpressDataApplication}
+         */
+        const app = container.get('ExpressDataApplication');
+        app.useService(RemoteServer);
+        serveApplication(container).then(() => {
+            return done();
+        });
+    });
+
     it('should create instance', () => {
         const context = new RemoteContext();
         expect(context).toBeTruthy()
